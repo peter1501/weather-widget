1a7b740eeafb517f72b1960a7233b39a0bd5b441
Peter Van Velsen <peterv.v@hotmail.com>
2016-12-23 13:33:55 +1000

    added typings

--- app/app.component.js
+++ app/app.component.js
@@ -15,6 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
+            moduleId: module.id,
             template: '<h1>Skeleton Project</h1>'
         }), 
         __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'my-app',
+    moduleId: module.id,
     template: '<h1>Skeleton Project</h1>'
 })
 
